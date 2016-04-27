2fc42f5aa38b1f15ee50d370184b252635db1d20
raorao <savinirs@gmail.com>
2016-04-27 10:31:00 -0700

    initial commit with data cleaning

---
+++ adult.R
@@ -0,0 +1,50 @@
+setwd("~/Documents/datascience")
+
+adult.data <- read.csv('adult.data', header = F)
+adult.test <- read.csv('adult.test', header = F, comment.char = '|')
+adult.names <- c("age","workclass","fnlwgt","education","education.num","marital.status","occupation","relationship","race","sex","capital.gain","capital.loss","hours.per.week","native.country", "fifty.k.status")
+
+set.nas <- function(frame, colname) {
+  index <- grep(paste('^', colname, '$', sep = ""), colnames(frame))
+  frame[frame[,index] == ' ?', index] <- NA
+  frame[,index] <- droplevels(frame[,index])
+  frame
+}
+
+prep.data.frame <- function(frame) {
+  #set colnames
+  colnames(frame) <- adult.names
+
+  #remove useless features
+  frame <- frame[, -c(3,5)]
+
+  #clean NAs
+  frame <- set.nas(frame, 'workclass')
+  frame <- set.nas(frame, 'occupation')
+  frame <- set.nas(frame, 'native.country')
+
+  # remove data points without occupation or workclass
+  frame <- frame[!(is.na(frame$occupation) & is.na(frame$workclass)), ]
+
+  #remove data points without country
+  frame <- frame[-(is.na(frame$native.country)), ]
+
+  # standardize fifty.k.status
+  levels(frame$fifty.k.status) <- c(" <=50K", " >50K")
+
+  # create martal.status.cleaned feature
+  frame$marital.status.cleaned = frame$marital.status
+  levels(frame$marital.status.cleaned) <- list(
+    never.married = c('Never-married'),
+    married = c('Married-civ-spouse', 'Married-AF-spouse'),
+    complicated = c('Married-spouse-absent', 'Separated', 'Divorced', 'Widowed')
+  )
+
+  frame
+}
+
+adult.data <- prep.data.frame(adult.data)
+adult.test <- prep.data.frame(adult.test)
+
+
+# adult.sample <- adult.data[sample(1:nrow(adult.data), 100),]
